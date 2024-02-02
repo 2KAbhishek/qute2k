3102065734abfd65ab08abfc738b264e3b81541a
Abhishek Keshri <iam2kabhishek@gmail.com>
2024-02-02 14:34:39 +0530

    revert: "feat: update user agent config"
    This reverts commit 94b1f34586f5573ba4c6f781bc0dcbeeef9a31fd.

--- config.py
+++ config.py
@@ -38,7 +38,11 @@ c.downloads.position = "bottom"
 c.downloads.location.suggestion = "both"
 
 # User agent
-c.content.headers.user_agent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36"
+config.set(
+    "content.headers.user_agent",
+    "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36",
+    "*",
+)
 
 # Dark mode
 c.colors.webpage.darkmode.enabled= True
