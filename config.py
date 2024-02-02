94b1f34586f5573ba4c6f781bc0dcbeeef9a31fd
Abhishek Keshri <iam2kabhishek@gmail.com>
2024-02-02 09:28:13 +0530

    feat: update user agent config

--- config.py
+++ config.py
@@ -29,11 +29,7 @@ c.auto_save.session = True
 c.url.auto_search = "naive"
 
 # User agent
-config.set(
-    "content.headers.user_agent",
-    "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36",
-    "*",
-)
+c.content.headers.user_agent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36"
 
 # Dark mode
 config.set("colors.webpage.darkmode.enabled", True)
